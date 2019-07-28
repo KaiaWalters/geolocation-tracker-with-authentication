3006b724d915f0c8a7f803230502e1b9561ba620
KaiaWalters <kaia.cwalters@gmail.com>
2019-07-28 14:55:16 -0400

    Complete and working project,minor issues

--- server.js
+++ server.js
@@ -4,14 +4,14 @@
 // get all the tools we need
 var express  = require('express');
 var app      = express();
-var port     = process.env.PORT || 8080;
+var port     = process.env.PORT || 8080; // what is a port
 const MongoClient = require('mongodb').MongoClient
 var mongoose = require('mongoose');
-var passport = require('passport');
+var passport = require('passport'); //used for authentication
 var flash    = require('connect-flash');
 
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
+var morgan       = require('morgan'); // logger logs everything that is happening n the server
+var cookieParser = require('cookie-parser');//remembers who you are
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
@@ -23,7 +23,7 @@ var db
 mongoose.connect(configDB.url, (err, database) => {
   if (err) return console.log(err)
   db = database
-  require('./app/routes.js')(app, passport, db);
+  require('./app/routes.js')(app, passport, db); //is a file made to hold the api , spits out a function
 }); // connect to our database
 
 //app.listen(port, () => {
